15a91628c076a189c615fa164fb3835cdc0f73e1
Colin Christ <colin.christ@icloud.com>
2023-04-20 13:48:59 +0200

    create project essentials
    create new project in eclipse and create essential project files

---
+++ src/hauptpaket/Marktplatz.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hauptpaket;
+
+/**
+ * @author colichri
+ *
+ */
+public class Marktplatz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
